a02af73d824c1e62a105da14c756651d7adeeb53
bee_keeper <ricbottomley@gmail.com>
2015-12-01 22:25:52 +0000

    testing

--- setup.py
+++ setup.py
@@ -22,13 +22,12 @@ setup(
         'Topic :: Internet',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
     ],
     install_requires=[
-        'django-allauth>=0.22',
         'django-braces>=1.8.0',
     ],
 )
